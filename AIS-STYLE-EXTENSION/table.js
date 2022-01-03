6696c6218b14c0338cf38e967e79da03a0856c60
3DRIK <art3drik@gmail.com>
2022-01-03 15:11:12 +0100

    v1.2

--- AIS-STYLE-EXTENSION/table.js
+++ AIS-STYLE-EXTENSION/table.js
@@ -2,17 +2,7 @@ window.onload = ()=>{
     if (window.location.href.indexOf("student/list.pl") !== -1) {
         var list1 = document.querySelectorAll('table#tmtab_1');
         list1.forEach((value)=>{
-            value.style.cssText = value.style.cssText+"width:93vw; min-width: 900px; max-width: 1400px;"
+            value.style.cssText = "display: block;width: 96%;overflow-x: auto;-ms-overflow-style: -ms-autohiding-scrollbar;margin-right: auto;margin-left: auto;"
         })
-
-        var list2 = document.querySelectorAll('#tmtab_1 td');
-        list2.forEach((value)=>{
-            value.style.cssText = value.style.cssText+"max-width: 55px; overflow: hidden; white-space: nowrap; padding: 3px !important;"
-        })
-        var list3 = document.querySelectorAll('#tmtab_1 th');
-        list3.forEach((value)=>{
-            value.style.cssText = value.style.cssText + "overflow: hidden;"
-        })
-       
     }
 };
