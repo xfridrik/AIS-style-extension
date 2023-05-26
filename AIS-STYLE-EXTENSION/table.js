638ab44aa57ae1d81041a1aa77ad3247667b11b0
Martin Fridrik <xfridrik@stuba.sk>
2023-05-26 23:03:36 +0200

    feat: version 1.3
    -dark table headers
    -dark inputs
    -some fixes

--- AIS-STYLE-EXTENSION/table.js
+++
@@ -1,8 +0,0 @@
-window.onload = ()=>{
-    if (window.location.href.indexOf("student/list.pl") !== -1) {
-        var list1 = document.querySelectorAll('table#tmtab_1');
-        list1.forEach((value)=>{
-            value.style.cssText = "display: block;width: 96%;overflow-x: auto;-ms-overflow-style: -ms-autohiding-scrollbar;margin-right: auto;margin-left: auto;"
-        })
-    }
-};
